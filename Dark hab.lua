361056f7fe40423c0befe983836e22e669bb9191
benzinmihail469-design <benzinmihail469@gmail.com>
2026-05-30 23:47:40 +0300

    Update Dark hab.lua

--- Dark hab.lua
+++ Dark hab.lua
@@ -1,3 +1,7 @@
 local CleanUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/benzinmihail469-design/Rararara/refs/heads/main/Esp.lua"))()
 
 local Window = CleanUI:CreateWindow("Dark Hub", "Dark") -- темы: "Dark", "Purple", "Blue", "Green"
+
+local MainTab = Window:AddTab("main")
+
+local CombatSection = MainTab:AddSection("walk speed")
